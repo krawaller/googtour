4beaaf3594e8c999c5e30af14bf8b63ec67365fc
KungD <david@krawaller.se>
2013-01-20 21:12:28 +0100

    img anim

--- src/tour.js
+++ src/tour.js
@@ -1,6 +1,6 @@
 if (window.location.search!=="?pure") {$("body").addClass("tour");}
 
-$("article").on("click",function(){
+$(".tour article").on("click",function(){
 	$el = $(this);
 	if (!$el.hasClass("showing")){
 		$(".showing").removeClass("showing").addClass("hiding");
