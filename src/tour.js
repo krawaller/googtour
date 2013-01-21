c8ed10eac4a96d8abebd9cbfabc59fd9458c924c
KungD <david@krawaller.se>
2013-01-21 17:25:54 +0100

    nonjq fix

--- src/tour.js
+++ src/tour.js
@@ -1,9 +1,11 @@
-if (window.location.search!=="?pure") {$("body").addClass("tour");}
-
-$(".tour article").on("click",function(){
-	$el = $(this);
-	if (!$el.hasClass("showing")){
-		$(".showing").removeClass("showing").addClass("hiding");
-		$el.removeClass("hiding").addClass("showing");
-	}
-});
+if (window.location.search!=="?pure") {
+	document.body.setAttribute("class","tour");
+	var currentarticle;
+	[].forEach.call(document.querySelectorAll("article"),function(a){
+		a.addEventListener("click",function(){
+			currentarticle && currentarticle.setAttribute("class","hiding");
+			a.setAttribute("class","showing");
+			currentarticle = a;
+		});
+	});
+}
